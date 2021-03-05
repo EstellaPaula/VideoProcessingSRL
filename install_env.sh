2f4c330ae345c497c2c045ff95eaecce565f94e6
Daniel Schipor <dschipor10@gmail.com>
2021-03-05 11:05:40 +0200

    rewritten transcode command

--- install_env.sh
+++ install_env.sh
@@ -3,4 +3,7 @@ sudo dnf install ffmpeg
 pip3 install pyyaml
 sudo dnf install python3-tkinter
 pip3 install PySimpleGUI
-pip3 install matplotlib 
+pip3 install matplotlib
+
+#ffprobe -i big-in.mkv -select_streams v -show_frames -show_entries frame=best_effort_timestamp_time > timestamps.txt
+#ffprobe -i big-in.mkv -show_frames -show_entries frame=pkt_pts_time -of csv=p=0 > timestamps.txt 
--- media_parser.py
+++ media_parser.py
@@ -1,10 +1,19 @@
 import subprocess, os
 import PySimpleGUI as sg
+import yaml
 
-VIDEO_ENCODERS = {"1":"copy", "2":"libx264", "3":"libx265", "4":"libvpx-vp9", "5":"libaom-av1"}
-
-presets = {"libx264", ["-c:v libx264 fast-speed", "-c:v libx264 slow-speed"]}
-AUDIO_ENCODERS = {"1":"copy"}
+def describe_list(list_name, list_values):
+    print("Options for", list_name, ":")
+    for i in range(len(list_values)):
+        print(i,": ", list_values[i])
+    return
+def select_from_list(list_name, list_values):
+    describe_list(list_name, list_values)
+    print("Select", list_name, "id:", end=" ")
+    value_id = int(input())
+    if value_id not in range(len(list_values)):
+        print("Warning, id not from list!")
+    return list_values[value_id]
 
 # params = [input_file, output_file, global_options, input_options, output_options]
 def call_ffmpeg(params, verbose = False):
@@ -21,53 +30,61 @@ def call_ffmpeg(params, verbose = False):
     return subprocess.call(args)
 
 # params = [input_file, output_file, global_options, input_options, output_options]
-def get_transcode_params(input_file, output_file, verbose = False):
+def get_transcode_params(in_file, out_file, vid_pres_file, aud_pres_file, verbose = False):
     # init params 
     global_options = ["-loglevel", "quiet"]
     input_options = []
     output_options = []
-    
-    # init global params
-    if (verbose):
-        global_options = ["-loglevel", "panic"]
-    # get video encoder
-    cv = "copy"
-    while True:
-        print("Video encoder options:", VIDEO_ENCODERS)
-        cv_idx = input("Select encoder: ")
-        if cv_idx in VIDEO_ENCODERS:
-            cv = VIDEO_ENCODERS[cv_idx]
-            break
-        else:
-            print("Wrong id, try again")
-    
-    # update output_options
-    output_options += ["-c:v", cv]
-    
-    # get audio encoder
-    ca = "copy"
-    while True:
-        print()
-        print("Audio encoder options:", AUDIO_ENCODERS)
-        ca_idx = input("Select encoder: ")
-        if ca_idx in AUDIO_ENCODERS:
-            ca = AUDIO_ENCODERS[ca_idx]
-            break
-        else:
-            print("Wrong id, try again")
-    
-    # update output_options
-    output_options += ["-c:a", ca]
-    
-    #return params
-    return [input_file, output_file, global_options, input_options, output_options]
+
+    # open yaml files and get presets
+    vfile = open(vid_pres_file, "r")
+    afile = open(aud_pres_file, "r")
+    video_presets = yaml.full_load(vfile)
+    audio_presets = yaml.full_load(afile)
+
+    # get video codec
+    output_options += ["-c:v"]
+    codecs = video_presets["-c:v"]
+    codec_list = list(codecs.keys())
+    codec = select_from_list("video codec", codec_list)
+    output_options += [codec]
+    # get video codec settings
+    codec_settings = codecs[codec]
+    if codec_settings:
+        for s_name in codec_settings:
+            setting = codec_settings[s_name]
+            s_value = select_from_list(s_name, setting)
+            if s_value != "ignore":
+                output_options += [s_name, s_value]
+
+    # get audio codec
+    output_options += ["-c:a"]
+    codecs = audio_presets["-c:a"]
+    codec_list = list(codecs.keys())
+    codec = select_from_list("audio codec", codec_list)
+    output_options += [codec]
+    # get audio codec settings
+    codec_settings = codecs[codec]
+    if codec_settings:
+        for s_name in codec_settings:
+            setting = codec_settings[s_name]
+            s_value = select_from_list(s_name, setting)
+            if s_value != "ignore":
+                output_options += [s_name, s_value]
+    # close yaml files
+    vfile.close()
+    afile.close()
+    return [in_file, out_file, global_options, input_options, output_options]
 
 def main():
     input_file = "in.mp4"
     output_file = "out.mkv"
+    video_presets_file = "presets/video_presets.yaml"
+    audio_presets_file = "presets/audio_presets.yaml"
 
-    params = get_transcode_params(input_file, output_file, False)
-    call_ffmpeg(params, True)
+    params = get_transcode_params(input_file, output_file, video_presets_file, audio_presets_file)
+    call_ffmpeg(params)
+
 main()
 
 
