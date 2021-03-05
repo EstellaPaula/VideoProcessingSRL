6259d35a239b2efb4c2918fb01e10466686a40f3
Daniel Schipor <dschipor10@gmail.com>
2021-03-05 15:58:10 +0200

    keyframe extraction using csv

--- media_parser.py
+++ media_parser.py
@@ -1,7 +1,16 @@
 import subprocess, os
 import PySimpleGUI as sg
+import numpy as np
 import yaml
-import json
+import csv
+
+DEFAULT_LOGLEVEL = ["-loglevel", "quiet"]
+
+# functions to interact with lists (may change)
+def get_timestamp_intervals(timestamps, number):
+    splitted = np.array_split
+
+
 
 # functions to interact with user (may change)
 def describe_list(list_name, list_values):
@@ -20,21 +29,30 @@ def select_from_list(list_name, list_values):
 # params = [input_file, output_file, global_options, input_options, output_options]
 def call_ffmpeg(params, verbose = False):
     [input_file, output_file, global_options, input_options, output_options] = params
-    # CREATE FFMPEG COMMAND
+    # create ffmpeg command
     # correct order: ffmpeg [global options] [input options] -i input_file [output options] output_file
     args = ["ffmpeg"] + global_options + input_options
-    args += ["-i", input_file] + output_options
-    args += [output_file]
+    args += ["-i", input_file] + output_options + [output_file]
     # show args
     if(verbose):
         print("Args:", args)
     # call ffmpeg
     return subprocess.call(args)
 
+# get ffmpeg params for breaking into chunks
+# params = [input_file, output_file, global_options, input_options, output_options]
+def get_chunk_break_params(in_file, chunk_number, timestamps):
+    # init params 
+    global_options = DEFAULT_LOGLEVEL
+    input_options = []
+    output_options = []
+    return
+
+# get ffmpeg params for transcoding opearation
 # params = [input_file, output_file, global_options, input_options, output_options]
 def get_transcode_params(in_file, out_file, vid_pres_file, aud_pres_file, verbose = False):
     # init params 
-    global_options = ["-loglevel", "quiet"]
+    global_options = DEFAULT_LOGLEVEL
     input_options = []
     output_options = []
 
@@ -78,41 +96,32 @@ def get_transcode_params(in_file, out_file, vid_pres_file, aud_pres_file, verbos
     afile.close()
     return [in_file, out_file, global_options, input_options, output_options]
 
-# implement command :
-# ffprobe -loglevel error -select_streams v:0 -show_entries packet=pts_time,flags /
-# -of csv=print_section=0 input.mp4 | awk -F',' '/K/ {print $1}' 
-def get_keyframes(in_file):
+# get a list of keyframe timestamps
+def get_keyframe_timestamps(in_file):
     # init command args
-    cmd_1 = "ffprobe -v quiet -loglevel error -select_streams v:0 -show_entries packet=pts_time,flags -of json"
-    args = cmd_1.split() + [in_file]
-    # make a temporary file
-    # call ffprobe and send output to temporary json
-    tmp_json = open("tmp/timestamps.json", "w")
-    subprocess.call(args, stdout = tmp_json)
-    #close temporary file
-    tmp_json.close()
-    # open json and get list of kframes from i-frames
-    k_timestamps = []
-    tmp_json = open("tmp/timestamps.json", "r")
-    json_data = json.load(tmp_json)
-    for frame_data in json_data["packets"]:
-        if frame_data["flags"][0] == 'K':
-            k_timestamps.append(frame_data["pts_time"])
-    #close temporary file
-    tmp_json.close()
-    print(k_timestamps)
-    
-    return k_timestamps
+    cmd_1 = "ffprobe -v quiet -loglevel error -select_streams v:0 -show_entries packet=pts_time,flags -of csv=print_section=0"
+    args_1 = cmd_1.split() + [in_file]
+    # call ffprobe, send i-frames to tmp csv file
+    subprocess.call(args_1, stdout = open("tmp/timestamps.csv", "w"))
+    # get keyframes from csv
+    timestamps = []
+    csv_reader = csv.reader(open("tmp/timestamps.csv", "r"))
+    for frame in csv_reader:
+        # check tag 
+        if frame[1][0] == 'K':
+            timestamps.append(frame[0])
+    print(timestamps)
+    return timestamps
 
 def main():
-    input_file = "input/in.mp4"
+    input_file = "input/big-in.mkv"
     output_file = "output/out.mkv"
     video_presets_file = "presets/video_presets.yaml"
     audio_presets_file = "presets/audio_presets.yaml"
 
     # params = get_transcode_params(input_file, output_file, video_presets_file, audio_presets_file)
     # call_ffmpeg(params)
-    get_keyframes(input_file)
+    get_keyframe_timestamps(input_file)
 
 main()
 
