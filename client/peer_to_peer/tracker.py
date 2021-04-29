72090481ebfc1513b3c4c206c0e32cfce9512508
daniel.schipor <daniel.schipor@stud.acs.upb.ro>
2021-04-29 18:11:59 +0300

    added tracker class

--- client/peer_to_peer/tracker.py
+++ client/peer_to_peer/tracker.py
@@ -19,7 +19,7 @@ class Tracker():
         """
             Required fields: none
             Optional fields: none
-            Returns: list of users
+            Returns: request code, request output
 
             Return a list of users
         """
@@ -28,10 +28,10 @@ class Tracker():
         url = self.home + "api/user"
         r = requests.get(url)
         if r.status_code == 200:
-            return r.json()
+            return r.status_code, r.json()
         else:
             print("Get users HTTP error:", r.status_code)
-            return r.json()
+            return r.status_code, r.json()
     
     def register_user(self, username, password):
         """
@@ -46,16 +46,16 @@ class Tracker():
         url = self.home + "api/register"
         r = requests.post(url, json={"username":username, "password":password})
         if r.status_code == 200:
-            return r.json()
+            return r.status_code, r.json()
         else:
             print("Register HTTP error:", r.status_code)
-            return r.json()
+            return r.status_code, r.json()
 
     def log_in(self, username, password):
         """
             Required fields: username, password
             Optional fields: none
-            Returns: error code or nothing
+            Returns: error code or token
 
             Logs in user and returns token
         """
@@ -65,11 +65,242 @@ class Tracker():
         r = requests.get(url, auth=(username, password))
         if r.status_code == 200:
             token = r.json()["token"]
-            return token
+            return r.status_code, token
         else:
             print("Log in HTTP error:", r.status_code)
-            return r.json()
+            return r.status_code, r.json()
 
-    def 
+    def get_boss_list(self):
+        """
+            Required fields: none
+            Optional fields: none
+            Returns: error code or nothing
+
+            Gets list of bosses
+        """
+
+        print("\nGet a list of bosses", flush=True)
+        url = self.home + "api/boss"
+        r = requests.get(url)
+        if r.status_code == 200:
+            return r.status_code, r.json()
+        else:
+            print("Get boss list HTTP error:", r.status_code)
+            return r.status_code, r.json()
+    
+    def get_worker_list(self):
+        """
+            Required fields: none
+            Optional fields: none
+            Returns: error code or nothing
+
+            Gets list of workers
+        """
+        print("\nGet a list of workers", flush=True)
+        url = self.home + "api/worker"
+        r = requests.get(url)
+        if r.status_code == 200:
+            return r.status_code, r.json()
+        else:
+            print("Get worker list HTTP error:", r.status_code)
+            return r.status_code, r.json()
+
+
+    def register_boss_session(self, hostname, token):
+        """
+            Required fields: hostname, token
+            Optional fields: none
+            Returns: boss instance id or error_code
+
+            Registers a boss session
+        """
+
+        print("\nRegister a boss session", flush=True)
+        url = self.home + "api/register/boss"
+        r = requests.post(url, headers={"Content-Type":"application/json", "x-access-tokens":token}, json={"ip_address":hostname})
+        if r.status_code == 200:
+            boss_id = r.json()["id"]
+            return r.status_code, boss_id
+        else:
+            print("Boss register HTTP error:", r.status_code)
+            return r.status_code, r.json()
+    
+    def register_worker_session(self, hostname, msg_port, file_port, speeds, token):
+        """
+            Required fields: hostname, msg_port, file_port, speeds, token
+            Optional fields: none
+            Returns: worker instance id or error_code
+
+            Registers a worker session
+        """
+
+        print("\nRegister a worker session", flush=True)
+        url = self.home + "api/register/worker"
+        r = requests.post(url, headers={"Content-Type":"application/json", "x-access-tokens":token},
+         json={"ip_address":hostname, "port_msg":msg_port, "port_file":file_port, "pp_x265":speeds["x265"],
+         "pp_vp9":speeds["vp9"], "pp_av1":speeds["av1"]})
+        if r.status_code == 200:
+            worker_id = r.json()["id"]
+            return r.status_code, worker_id
+        else:
+            print("Worker register HTTP error:", r.status_code)
+            return r.status_code, r.json()
+
+    def update_x265_speed(self, worker_id, new_speed, token):
+        """
+            Required fields: worker_id, new_speed, token
+            Optional fields: none
+            Returns: nothing or error_code
+
+            Updates x265 speed
+        """
+
+        print("\nUpdate x265 speed", flush=True)
+        url = self.home + "api/worker/pp_x265"
+        r = requests.put(url, headers={"Content-Type":"application/json", "x-access-tokens":token},
+         json={"id":worker_id, "pp_x265":new_speed})
+        if r.status_code == 200:
+            return r.status_code, r.json()
+        else:
+            print("Worker x265 speed update HTTP error:", r.status_code)
+            return r.status_code, r.json()
+    
+    def update_vp9_speed(self, worker_id, new_speed, token):
+        """
+            Required fields: worker_id, new_speed, token
+            Optional fields: none
+            Returns: nothing or error_code
+
+            Updates vp9 speed
+        """
+
+        print("\nUpdate vp9 speed", flush=True)
+        url = self.home + "api/worker/pp_vp9"
+        r = requests.put(url, headers={"Content-Type":"application/json", "x-access-tokens":token},
+         json={"id":worker_id, "pp_vp9":new_speed})
+        if r.status_code == 200:
+            return r.status_code, r.json()
+        else:
+            print("Worker vp9 speed update HTTP error:", r.status_code)
+            return r.status_code, r.json()
+
+    def update_av1_speed(self, worker_id, new_speed, token):
+        """
+            Required fields: worker_id, new_speed, token
+            Optional fields: none
+            Returns: nothing or error_code
+
+            Updates av1 speed
+        """
+
+        print("\nUpdate av1 speed", flush=True)
+        url = self.home + "api/worker/pp_av1"
+        r = requests.put(url, headers={"Content-Type":"application/json", "x-access-tokens":token},
+         json={"id":worker_id, "pp_av1":new_speed})
+        if r.status_code == 200:
+            return r.status_code, r.json()
+        else:
+            print("Worker av1 speed update HTTP error:", r.status_code)
+            return r.status_code, r.json()
+
+    def update_speed(self, worker_id, speeds, token):
+        """
+            Required fields: worker_id, speeds
+            Optional fields: none
+            Returns: nothing or error_code
+
+            Updates speeds
+        """
+
+        for codec in speeds:
+            new_speed = speeds[codec]
+            if codec == "x265":
+                self.update_x265_speed(worker_id, new_speed, token)
+            elif codec == "vp9":
+                self.update_vp9_speed(worker_id, new_speed, token)
+            else:
+                self.update_av1_speed(worker_id, new_speed, token)
+        return
+
+    def get_workers(self, boss_id, token):
+        """
+            Required fields: boss_id, token
+            Optional fields: none
+            Returns: list of workers
+
+            Get a list of workers
+        """
+
+        print("\nGet workers", flush=True)
+        url = self.home + "api/boss/job"
+        r = requests.get(url, headers={"Content-Type":"application/json", "x-access-tokens":token},
+         json={"id":boss_id})
+        if r.status_code == 200:
+            workers = r.json()
+            return r.status_code, workers
+        else:
+            print("Get workers HTTP error:", r.status_code)
+            return r.status_code, r.json()
+
+    def boss_finish(self, boss_id, token):
+        """
+            Required fields: boss_id, token
+            Optional fields: none
+            Returns: nothing
+
+            Notifies tracker that boss finished job
+        """
+
+        print("\nBoss finish", flush=True)
+        url = self.home + "api/boss/status"
+        r = requests.put(url, headers={"Content-Type":"application/json", "x-access-tokens":token},
+         json={"id":boss_id})
+        if r.status_code == 200:
+            return r.status_code, r.json()
+        else:
+            print("Boss finish HTTP error:", r.status_code)
+            return r.status_code, ""
+    
+    def worker_finish(self, worker_id, token):
+        """
+            Required fields: worker_id, token
+            Optional fields: none
+            Returns: nothing
+
+            Notifies tracker that boss finished job
+        """
+
+        print("\nWorker finish", flush=True)
+        url = self.home + "api/worker/status"
+        r = requests.put(url, headers={"Content-Type":"application/json", "x-access-tokens":token},
+         json={"id":worker_id})
+        if r.status_code == 200:
+            return r.status_code, r.json()
+        else:
+            print("Worker finish HTTP error:", r.status_code)
+            return r.status_code, r.json()
+    
+    def get_worker_owner(self, worker_id, token):
+        """
+            Required fields: worker_id, token
+            Optional fields: none
+            Returns: nothing
+
+            Get a worker's legitimate boss
+        """
+
+        print("\nGet worker owner", flush=True)
+        url = self.home + "api/worker/owner"
+        r = requests.get(url, headers={"Content-Type":"application/json", "x-access-tokens":token},
+         json={"id":worker_id})
+        if r.status_code == 200:
+            return r.status_code, r.json()
+        else:
+            print("Worker owner HTTP error:", r.status_code)
+            return r.status_code, ""
+
+
+    
+
     
 
--- client/stub.py
+++ client/stub.py
@@ -1,15 +1,42 @@
 from peer_to_peer.tracker import Tracker
 
 def main():
-    tracker = Tracker("localhost", "5000")
+    tracker = Tracker(hostname="localhost", port=5000)
     # get users
-    users = tracker.get_users()
+    ret_code, users = tracker.get_users()
     print(users)
+    # get boss list
+    ret_code, bosses = tracker.get_boss_list()
+    print(bosses)
+    # get worker list
+    ret_code, workers = tracker.get_worker_list()
+    print(workers)
     # register user
-    out = tracker.register_user("sam", "sam")
+    ret_code, out = tracker.register_user(username="george",password="george")
     print(out)
     # log in user
-    token = tracker.log_in("sam", "sam")
+    ret_code, token = tracker.log_in(username="george", password="george")
+    print(token)
+    # register boss session
+    ret_code, boss_id = tracker.register_boss_session(hostname="localhost", token=token)
+    print(boss_id)
+    # register worker session
+    speeds = {"x265":0, "vp9":0, "av1":0}
+    ret_code, worker_id = tracker.register_worker_session(hostname="localhost", 
+    msg_port=50001, file_port=50002, speeds=speeds, token=token)
+    print(worker_id)
+    # update speed for codecs
+    tracker.update_speed(worker_id=1, speeds={"x265":1, "vp9":1}, token=token)
+    # get workers
+    tracker.get_workers(boss_id=1, token=token)
+    # get worker owner
+    ret_code, owner_ip = tracker.get_worker_owner(worker_id=worker_id, token=token)
+    print(owner_ip)
+    # finish boss
+    ret_code, out = tracker.boss_finish(boss_id=boss_id, token=token)
+    print(out)
+    # finish worker
+    ret_code, out = tracker.boss_finish(boss_id=boss_id, token=token)
     print(out)
 
     return 0
